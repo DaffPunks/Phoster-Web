49b443b14920fecb72085c41e99767b730a0fca9
DaffPunks <sawavas2@gmail.com>
2018-10-12 17:39:03 +0700

    (+) Add example containers

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default class App extends React.Component {
   // render
   render() {
     return (
-      <div className="container">
+      <div>
         {this.props.children}
       </div>
     );
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,13 @@
 import React from "react";
 import './style.scss';
-import Header from '../../components/Header';
 
 // Home page component
 export default class Home extends React.Component {
   // render
   render() {
     return (
-      <div className="page-home">
-        <Header/>
+      <div className="header">
+        a
       </div>
     );
   }
