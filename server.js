453c3cc7c7cef2ed958147d3311e7e7b354514cb
DaffPunks <sawavas2@gmail.com>
2018-10-13 08:35:38 +0700

    (U) Update heroku scripts

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/my'));
+app.use(express.static(__dirname + '/dpublic'));
 
 app.get('/*', function(req,res) {
 
-    res.sendFile(path.join(__dirname+'/dist/my/index.html'));
+    res.sendFile(path.join(__dirname+'/public/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
